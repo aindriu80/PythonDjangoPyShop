9b1c876b6fa3ec553d09fd7343af250e87e009af
aindriu80 <aindriu80@gmail.com>
2019-03-20 21:28:43 +0000

    Adding templates

--- products/views.py
+++ products/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
+
 
 # /products ->
 
 def index(request):
-    return HttpResponse('Hello World')
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
 
 
 def products_new(request):
