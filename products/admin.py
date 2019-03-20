de2bbc32d69772d78d6120f9586fcaa3978c73ca
aindriu80 <aindriu80@gmail.com>
2019-03-20 21:17:12 +0000

    Adding discounts section

--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Offer
 
 
 class ProductAdmin(admin.ModelAdmin):
         list_display = ('name','price','stock')
 
+class OffersAdmin(admin.ModelAdmin):
+        list_display = ('code','discount')
 
+
+admin.site.register(Offer, OffersAdmin)
 admin.site.register(Product, ProductAdmin)
